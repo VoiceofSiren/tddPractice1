6adcc64a0f05c7feb79df65c4cf91c70c88a2045
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-06-15 15:36:33 +0900

    feat: define PostControllerTest.test1()

--- src/test/java/com/example/tdd1/post/controller/PostControllerTest.java
+++ src/test/java/com/example/tdd1/post/controller/PostControllerTest.java
@@ -1,12 +1,21 @@
 package com.example.tdd1.post.controller;
 
+import com.example.tdd1.post.dto.PostRequestDto;
 import com.example.tdd1.post.service.PostService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // Controller 테스트를 위한 애너테이션
 @WebMvcTest(controllers = PostController.class)
@@ -20,5 +29,25 @@ class PostControllerTest {
     PostService postService;
 
     // api method
+    @Test
+    @DisplayName("POST /post 테스트 코드 작성")
+    void test1() throws Exception {
 
+        // given
+        PostRequestDto postRequestDto = new PostRequestDto();
+        postRequestDto.setTitle("title");
+        postRequestDto.setContent("content");
+
+        // when & then
+        mockMvc.perform(
+                // POST 요청
+                post("post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(postRequestDto))
+                )
+                // 응답
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+    }
 }
