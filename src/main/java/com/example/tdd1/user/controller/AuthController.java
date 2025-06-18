a9f2c595e0e3a6288fb19c325aa5e0c8a164b392
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-06-18 15:21:29 +0900

    feat: define POST /auth/signUp and UserCreateRequestDto

--- src/main/java/com/example/tdd1/user/controller/AuthController.java
+++ src/main/java/com/example/tdd1/user/controller/AuthController.java
@@ -1,11 +1,16 @@
 package com.example.tdd1.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
-
+    @PostMapping("/auth/signUp")
+    public ResponseEntity<?> signUp() {
+        throw new  UnsupportedOperationException("테스트 코드 작성 중");
+    }
 }
