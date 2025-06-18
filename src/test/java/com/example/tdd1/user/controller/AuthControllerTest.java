e7be7186b3fa2deb746a68c97fca70cb5a1af0b4
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-06-18 18:14:19 +0900

    refactor: change package

--- src/test/java/com/example/tdd1/user/controller/AuthControllerTest.java
+++ src/test/java/com/example/tdd1/user/controller/AuthControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.tdd1.user.controller;
 
+import com.example.tdd1.user.controller.api.AuthController;
 import com.example.tdd1.user.dto.UserCreateRequestDto;
+import com.example.tdd1.user.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,20 +10,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+// Controller 테스트를 위한 애너테이션
 @WebMvcTest(controllers = AuthController.class)
 class AuthControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockitoBean
+    UserService userService;
 
     @Test
     @WithMockUser(username = "admin", roles = {"ADMIN"})
