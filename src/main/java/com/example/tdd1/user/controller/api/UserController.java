d6b2d504a8887f1db41f68445b08e221eb0daf4f
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-06-19 18:00:30 +0900

    test: test1() for unit test passes

--- src/main/java/com/example/tdd1/user/controller/api/UserController.java
+++ src/main/java/com/example/tdd1/user/controller/api/UserController.java
@@ -1,17 +1,28 @@
 package com.example.tdd1.user.controller.api;
 
+import com.example.tdd1.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/users")
     public ResponseEntity<?> readUsers() {
 
         // TODO: 단위 테스트 코드 작성
 
-        throw new UnsupportedOperationException("테스트 코드 작성 중");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        return new ResponseEntity<>(httpHeaders, HttpStatus.OK);
     }
 }
--- src/test/java/com/example/tdd1/user/controller/UserControllerTest.java
+++ src/test/java/com/example/tdd1/user/controller/UserControllerTest.java
@@ -1,13 +1,45 @@
 package com.example.tdd1.user.controller;
 
+import com.example.tdd1.user.controller.api.UserController;
+import com.example.tdd1.user.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = UserControllerTest.class)
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// Controller 테스트 지원
+@WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockitoBean
+    UserService userService;
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @DisplayName("GET /users HTTP 상태 코드 확인용 테스트 코드")
+    void test1() throws Exception {
+
+        // given
+
+        // when & then
+        mockMvc.perform(
+                get("/users")
+                        .with(csrf())
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
 
 }
