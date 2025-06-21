68c4b35448e1a09f0aca33ed31b4483ea35477ae
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-06-21 14:51:57 +0900

    feat: define logIn API

--- src/main/java/com/example/tdd1/user/controller/api/AuthController.java
+++ src/main/java/com/example/tdd1/user/controller/api/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.tdd1.user.controller.api;
 
 import com.example.tdd1.user.dto.request.UserCreateRequestDto;
+import com.example.tdd1.user.dto.request.UserLoginRequestDto;
 import com.example.tdd1.user.dto.response.UserCreateResponseDto;
 import com.example.tdd1.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +28,12 @@ public class AuthController {
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 
         return new ResponseEntity<>(userCreateResponseDto, httpHeaders, HttpStatus.OK);
+    }
+
+    @PostMapping("/auth/logIn")
+    public ResponseEntity<?> logIn(@RequestBody UserLoginRequestDto userLoginRequestDto) {
+
+        throw new UnsupportedOperationException("테스트 코드 작성 중");
     }
 
 
---
+++ src/main/java/com/example/tdd1/user/dto/request/UserLoginRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.tdd1.user.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UserLoginRequestDto {
+    private String username;
+    private String password;
+}
