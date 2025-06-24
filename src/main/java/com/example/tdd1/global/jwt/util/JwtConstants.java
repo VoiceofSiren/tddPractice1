f6ab1e1f50f4d822787949762ef6940b14fea6d5
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-06-24 20:06:01 +0900

    chore: change time values to test simply

--- src/main/java/com/example/tdd1/global/jwt/util/JwtConstants.java
+++ src/main/java/com/example/tdd1/global/jwt/util/JwtConstants.java
@@ -4,6 +4,6 @@ public class JwtConstants {
 
     private JwtConstants() {} // 생성자 private 처리로 인스턴스화 방지
 
-    public static final long ACCESS_TOKEN_EXPIRATION = 1 * 1 * 30 * 1000L;    // 30초
-    public static final long REFRESH_TOKEN_EXPIRATION = 1 * 24 * 60 * 60 * 1000; // 1일
+    public static final long ACCESS_TOKEN_EXPIRATION = 1 * 1 * 20 * 1000L;    // 20초
+    public static final long REFRESH_TOKEN_EXPIRATION = 1 * 1 * 2 * 60 * 1000L; // 2분
 }
